3decf0678531c80a99dbc46d357c6f10c4ebdf65
1335382915 <1335382915@qq.com>
2018-07-18 23:21:30 +0800

    add mongodb

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
 const router = require("./router/index");
@@ -9,6 +9,31 @@ app.use((req, res, next) => {
 })
 
 app.use('/api', router.foodRouter);
+
+app.get('/', (req, res) => {
+    // mongodb.MongoClient.connect("mongodb://localhost/db",function(err, db){
+    //     if(!err){
+    //         var dbase = db.db("runoob");
+    //         dbase.createCollection('site', function (err, res) {
+    //             if (err) throw err;
+    //             console.log("创建集合!");
+    //             db.close();
+    //         });
+    //     }
+    //     else {
+    //         console.log(err);
+    //     }
+    // })
+})
+
+mongoose.connect("mongodb://localhost/db", function(err, db) {
+    if(err) {
+        console.log("连接失败");
+    }
+    else {
+        console.log("连接成功")
+    }
+})
 
 app.listen(8999, () => {
     console.log("server created!");
